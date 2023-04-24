abb3bb36be976afd974d50221020dbb71c5e3f5c
unknown <aso.mansoury@gmail.com>
2023-04-24 17:21:06 +0430

    create body section

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from 'next/head';
 import Header from '@/components/Header';
-
+import {MagnifyingGlassIcon,MicrophoneIcon} from "@heroicons/react/24/solid";
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -16,6 +16,27 @@ export default function Home() {
       
         {/*Header */}
         <Header/>
+
+        {/*Body */}
+        <form className='flex flex-col items-center mt-40'>
+          <Image 
+            src="https://www.vectorlogo.zone/logos/google/google-tile.svg"
+            width={300}
+            height={100}
+            objectFit='cover'
+          ></Image>
+
+          <div className='flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl'>
+            <MagnifyingGlassIcon className='h-5 text-gray-500 mr-3'></MagnifyingGlassIcon>
+            <input className='flex-grow focus:outline-none' type="text"></input>
+            <MicrophoneIcon className='h-5'></MicrophoneIcon>
+          </div>
+
+          <div className='flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center'>
+            <button className='btn'>Google Search</button>
+            <button className='btn'>I'm Feeling Lucky</button>
+          </div>
+        </form>
     </div>
 
 
