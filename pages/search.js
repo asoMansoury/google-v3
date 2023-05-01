1d7965336b763822a09e75ca1ac1b86623ff4158
unknown <aso.mansoury@gmail.com>
2023-05-02 01:39:21 +0430

    Create pagination component

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,5 @@
 import Response from "@/Response";
+import PaginationButtons from "@/components/PaginationButtons";
 import SearchHeader from "@/components/SearchHeader";
 import SearchResults from "@/components/SearchResults";
 import Head from "next/head";
@@ -18,14 +19,17 @@ export default function search({results}) {
         
         {/*Searh Results */}
         <SearchResults results= {results}></SearchResults>
+
+        <PaginationButtons></PaginationButtons>
     </div>
   )
 }
 
 export async function getServerSideProps(context){
-  const mockData = true;
+  const startIndex = context.query.start || "1";
+  const mockData = false;
   const data =mockData? Response : await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}&start=${startIndex}`
   ).then((response)=>response.json());
   return {
     props:{
