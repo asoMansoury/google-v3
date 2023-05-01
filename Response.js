9d9ad633b4dc6a22ea0a0e4661cd967ad7f55099
unknown <aso.mansoury@gmail.com>
2023-05-01 16:25:24 +0430

    get data from google api and create mock data

---
+++ Response.js
@@ -0,0 +1,248 @@
+export default{
+    "kind": "customsearch#search",
+    "url": {
+      "type": "application/json",
+      "template": "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"
+    },
+    "queries": {
+      "request": [
+        {
+          "title": "Google Custom Search - اتنا",
+          "totalResults": "101000000",
+          "searchTerms": "اتنا",
+          "count": 10,
+          "startIndex": 1,
+          "inputEncoding": "utf8",
+          "outputEncoding": "utf8",
+          "safe": "off",
+          "cx": "815ba729cd5d54436",
+          "searchType": "image"
+        }
+      ],
+      "nextPage": [
+        {
+          "title": "Google Custom Search - اتنا",
+          "totalResults": "101000000",
+          "searchTerms": "اتنا",
+          "count": 10,
+          "startIndex": 11,
+          "inputEncoding": "utf8",
+          "outputEncoding": "utf8",
+          "safe": "off",
+          "cx": "815ba729cd5d54436",
+          "searchType": "image"
+        }
+      ]
+    },
+    "context": {
+      "title": "www.google.com"
+    },
+    "searchInformation": {
+      "searchTime": 0.372881,
+      "formattedSearchTime": "0.37",
+      "totalResults": "101000000",
+      "formattedTotalResults": "101,000,000"
+    },
+    "items": [
+      {
+        "kind": "customsearch#result",
+        "title": "صرف اتنا کہنا تھا یوں مان نہیں توڑا کرن کرتے ! | Love poetry ...",
+        "htmlTitle": "صرف <b>اتنا</b> کہنا تھا یوں مان نہیں توڑا کرن کرتے ! | Love poetry ...",
+        "link": "https://i.pinimg.com/474x/09/4f/1b/094f1ba5e0637812b3f7b69f2cd77379.jpg",
+        "displayLink": "www.pinterest.com",
+        "snippet": "صرف اتنا کہنا تھا یوں مان نہیں توڑا کرن کرتے ! | Love poetry ...",
+        "htmlSnippet": "صرف <b>اتنا</b> کہنا تھا یوں مان نہیں توڑا کرن کرتے ! | Love poetry ...",
+        "mime": "image/jpeg",
+        "fileFormat": "image/jpeg",
+        "image": {
+          "contextLink": "https://www.pinterest.com/pin/1089308228595394929/",
+          "height": 626,
+          "width": 368,
+          "byteSize": 13411,
+          "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy1SywuprgrD5OffOC_UMYwMgiYaazv0CZ8YN9caES5aArTdWgHKyzorM&s",
+          "thumbnailHeight": 136,
+          "thumbnailWidth": 80
+        }
+      },
+      {
+        "kind": "customsearch#result",
+        "title": "نتاشہ وقاص کے ساتھ اتنا برا کیسے ہوا😱@minitreevlog3381 - YouTube",
+        "htmlTitle": "نتاشہ وقاص کے ساتھ <b>اتنا</b> برا کیسے ہوا😱@minitreevlog3381 - YouTube",
+        "link": "https://i.ytimg.com/vi/q9YQUYR7hPc/hqdefault.jpg",
+        "displayLink": "www.youtube.com",
+        "snippet": "نتاشہ وقاص کے ساتھ اتنا برا کیسے ہوا😱@minitreevlog3381 - YouTube",
+        "htmlSnippet": "نتاشہ وقاص کے ساتھ <b>اتنا</b> برا کیسے ہوا😱@minitreevlog3381 - YouTube",
+        "mime": "image/jpeg",
+        "fileFormat": "image/jpeg",
+        "image": {
+          "contextLink": "https://www.youtube.com/watch?v=q9YQUYR7hPc",
+          "height": 360,
+          "width": 480,
+          "byteSize": 27152,
+          "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbBbG7qNm9E_f5eT6AW8XAnNRba9W1Bm7Q4j9lmM3ClsrR2UNBIA4z5jg&s",
+          "thumbnailHeight": 97,
+          "thumbnailWidth": 129
+        }
+      },
+      {
+        "kind": "customsearch#result",
+        "title": "اور عنقریب تمہارا رب تم کو اتنا دے گا کہ تم خوش ہو جاؤ گے ...",
+        "htmlTitle": "اور عنقریب تمہارا رب تم کو <b>اتنا</b> دے گا کہ تم خوش ہو جاؤ گے ...",
+        "link": "https://i.pinimg.com/736x/72/7e/4d/727e4d68ec03cb701aa2e96a381a4cc2.jpg",
+        "displayLink": "www.pinterest.com",
+        "snippet": "اور عنقریب تمہارا رب تم کو اتنا دے گا کہ تم خوش ہو جاؤ گے ...",
+        "htmlSnippet": "اور عنقریب تمہارا رب تم کو <b>اتنا</b> دے گا کہ تم خوش ہو جاؤ گے ...",
+        "mime": "image/jpeg",
+        "fileFormat": "image/jpeg",
+        "image": {
+          "contextLink": "https://www.pinterest.com/pin/802696333590909295/",
+          "height": 619,
+          "width": 640,
+          "byteSize": 8910,
+          "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-m2r38yuur9ZIYE9nMFB9O8syHAPtkjOqn3Ul7BDxc3shlOaqTYnZg0Dd&s",
+          "thumbnailHeight": 133,
+          "thumbnailWidth": 137
+        }
+      },
+      {
+        "kind": "customsearch#result",
+        "title": "Poster - Arabic.Design رَبَّنَا آتِنَا فِي الدُّنْيَا حَسَنَةً ...",
+        "htmlTitle": "Poster - Arabic.Design رَبَّنَا <b>آتِنَا</b> فِي الدُّنْيَا حَسَنَةً ...",
+        "link": "https://i0.wp.com/arabic.design/wp-content/uploads/2021/05/%D8%B1%D8%A8%D9%86%D8%A7-%D8%A7%D8%AA%D9%86%D8%A7-%D9%81%D9%8A-%D8%A7%D9%84%D8%AF%D9%86%D9%8A%D8%A7-%D8%AD%D8%B3%D9%86%D8%A9-Square-Poster-Nihad-Mockup-1.jpg?fit=1440%2C900&ssl=1",
+        "displayLink": "arabic.design",
+        "snippet": "Poster - Arabic.Design رَبَّنَا آتِنَا فِي الدُّنْيَا حَسَنَةً ...",
+        "htmlSnippet": "Poster - Arabic.Design رَبَّنَا <b>آتِنَا</b> فِي الدُّنْيَا حَسَنَةً ...",
+        "mime": "image/jpeg",
+        "fileFormat": "image/jpeg",
+        "image": {
+          "contextLink": "https://arabic.design/shop/print/print-art/poster/",
+          "height": 900,
+          "width": 1440,
+          "byteSize": 278253,
+          "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJYXDPf2YYwkrMYg1FeD9mNjOw_VjjVeDKdKPioRTE3HkRjhOQEZA7VQ&s",
+          "thumbnailHeight": 94,
+          "thumbnailWidth": 150
+        }
+      },
+      {
+        "kind": "customsearch#result",
+        "title": "ربنا اتنا في الدنيا حسنة وفي الاخرة حسنة وقنا عذاب النار | Good ...",
+        "htmlTitle": "ربنا <b>اتنا</b> في الدنيا حسنة وفي الاخرة حسنة وقنا عذاب النار | Good ...",
+        "link": "https://i.pinimg.com/564x/f1/7b/ee/f17bee631452e471376db53f97a1964b--design.jpg",
+        "displayLink": "www.pinterest.com",
+        "snippet": "ربنا اتنا في الدنيا حسنة وفي الاخرة حسنة وقنا عذاب النار | Good ...",
+        "htmlSnippet": "ربنا <b>اتنا</b> في الدنيا حسنة وفي الاخرة حسنة وقنا عذاب النار | Good ...",
+        "mime": "image/jpeg",
+        "fileFormat": "image/jpeg",
+        "image": {
+          "contextLink": "https://www.pinterest.com/pin/537828380477682467/",
+          "height": 808,
+          "width": 540,
+          "byteSize": 43169,
+          "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQblT0PScPjJ-AEEfrUykD8L8n4X7lJ58oqrEuN8ELRL-F5yMbD_p40SN0&s",
+          "thumbnailHeight": 143,
+          "thumbnailWidth": 96
+        }
+      },
+      {
+        "kind": "customsearch#result",
+        "title": "Qais Ulfat - Imshab Az Bada Kharabam And Tum Itna | قیس الفت ...",
+        "htmlTitle": "Qais Ulfat - Imshab Az Bada Kharabam And Tum Itna | قیس الفت ...",
+        "link": "https://i.ytimg.com/vi/sziKUqdGYuE/maxresdefault.jpg",
+        "displayLink": "www.youtube.com",
+        "snippet": "Qais Ulfat - Imshab Az Bada Kharabam And Tum Itna | قیس الفت ...",
+        "htmlSnippet": "Qais Ulfat - Imshab Az Bada Kharabam And Tum Itna | قیس الفت ...",
+        "mime": "image/jpeg",
+        "fileFormat": "image/jpeg",
+        "image": {
+          "contextLink": "https://www.youtube.com/watch?v=sziKUqdGYuE",
+          "height": 720,
+          "width": 1280,
+          "byteSize": 96028,
+          "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZjaVf2kukMxSThaswNavY3krptimniuQCXzXn_zJwBc7LIvs_3ONN6Cg&s",
+          "thumbnailHeight": 84,
+          "thumbnailWidth": 150
+        }
+      },
+      {
+        "kind": "customsearch#result",
+        "title": "اتنا هرندی (hrndyatna) - Profile | Pinterest",
+        "htmlTitle": "<b>اتنا</b> هرندی (hrndyatna) - Profile | Pinterest",
+        "link": "https://i.pinimg.com/1200x/21/0d/7f/210d7fa96b95219c5eaab8290874c0a2.jpg",
+        "displayLink": "www.pinterest.com",
+        "snippet": "اتنا هرندی (hrndyatna) - Profile | Pinterest",
+        "htmlSnippet": "<b>اتنا</b> هرندی (hrndyatna) - Profile | Pinterest",
+        "mime": "image/jpeg",
+        "fileFormat": "image/jpeg",
+        "image": {
+          "contextLink": "https://www.pinterest.com/hrndyatna/",
+          "height": 600,
+          "width": 600,
+          "byteSize": 57412,
+          "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9uTP1_NOwXMgSjMZsgWQCesKwuUx4SIdYgh1ML-YAgP0IyFHlrsYmBRY&s",
+          "thumbnailHeight": 135,
+          "thumbnailWidth": 135
+        }
+      },
+      {
+        "kind": "customsearch#result",
+        "title": "Pathani Village vlog|| اس بہن کا بہت بہت شکریہ جس نے میرا اتنا ...",
+        "htmlTitle": "Pathani Village vlog|| اس بہن کا بہت بہت شکریہ جس نے میرا <b>اتنا</b> ...",
+        "link": "https://i.ytimg.com/vi/m3pHqz-XefE/sddefault.jpg",
+        "displayLink": "www.youtube.com",
+        "snippet": "Pathani Village vlog|| اس بہن کا بہت بہت شکریہ جس نے میرا اتنا ...",
+        "htmlSnippet": "Pathani Village vlog|| اس بہن کا بہت بہت شکریہ جس نے میرا <b>اتنا</b> ...",
+        "mime": "image/jpeg",
+        "fileFormat": "image/jpeg",
+        "image": {
+          "contextLink": "https://www.youtube.com/watch?v=m3pHqz-XefE",
+          "height": 480,
+          "width": 640,
+          "byteSize": 74174,
+          "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZPzNFKyna0fvLdAA8Zawgi6YUyctZOQXXdLA3Wv6btwhbrG-GjOr3pA&s",
+          "thumbnailHeight": 103,
+          "thumbnailWidth": 137
+        }
+      },
+      {
+        "kind": "customsearch#result",
+        "title": "وہ اتنا دلکش ہے 🌸 | Emotional poetry, Poetry words, Love poetry urdu",
+        "htmlTitle": "وہ <b>اتنا</b> دلکش ہے 🌸 | Emotional poetry, Poetry words, Love poetry urdu",
+        "link": "https://i.pinimg.com/736x/18/31/b3/1831b3283efaaa8b84cd6c7ebb536fda.jpg",
+        "displayLink": "www.pinterest.com",
+        "snippet": "وہ اتنا دلکش ہے 🌸 | Emotional poetry, Poetry words, Love poetry urdu",
+        "htmlSnippet": "وہ <b>اتنا</b> دلکش ہے 🌸 | Emotional poetry, Poetry words, Love poetry urdu",
+        "mime": "image/jpeg",
+        "fileFormat": "image/jpeg",
+        "image": {
+          "contextLink": "https://www.pinterest.com/pin/730849845756023574/",
+          "height": 1280,
+          "width": 720,
+          "byteSize": 22073,
+          "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHuypnsTsMOjaQSzICd9W-85rd1AEM6_L8KmlX4e5o1JX-nAe8e6OrzGA&s",
+          "thumbnailHeight": 150,
+          "thumbnailWidth": 84
+        }
+      },
+      {
+        "kind": "customsearch#result",
+        "title": "ربنا آتنا من لدنك رحمة وهيئ لنا من أمرنا رشدا | رَبَّنَا آتِ… | Flickr",
+        "htmlTitle": "ربنا <b>آتنا</b> من لدنك رحمة وهيئ لنا من أمرنا رشدا | رَبَّنَا آتِ… | Flickr",
+        "link": "https://live.staticflickr.com/65535/49109132167_be6cb9c944_b.jpg",
+        "displayLink": "www.flickr.com",
+        "snippet": "ربنا آتنا من لدنك رحمة وهيئ لنا من أمرنا رشدا | رَبَّنَا آتِ… | Flickr",
+        "htmlSnippet": "ربنا <b>آتنا</b> من لدنك رحمة وهيئ لنا من أمرنا رشدا | رَبَّنَا آتِ… | Flickr",
+        "mime": "image/jpeg",
+        "fileFormat": "image/jpeg",
+        "image": {
+          "contextLink": "https://www.flickr.com/photos/156040355@N08/49109132167",
+          "height": 1024,
+          "width": 1024,
+          "byteSize": 112177,
+          "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQErLz2_Jk4PgGWwi8tyhOI1xSv9S6WsnihLM5VE2FMj0M6S3iJH32FGd7v&s",
+          "thumbnailHeight": 150,
+          "thumbnailWidth": 150
+        }
+      }
+    ]
+  }
